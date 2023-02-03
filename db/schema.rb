d541de879ed049ae7216ee3f045b08b924898860
hiep464 <luuhiep2309@gmail.com>
2023-02-03 19:10:47 +0000

    update add_detail_book

--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2023_01_31_041941) do
     t.integer "number_of_pages"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "detail"
   end
 
   create_table "users", force: :cascade do |t|
